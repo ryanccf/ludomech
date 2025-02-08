ca8d7b0d35dd0e566dd4105ade101925e9c09e58
Scott Westover <scottwestover2006@gmail.com>
2025-02-08 11:22:43 -0500

    feat: player-setup finished code
    Signed-off-by: Scott Westover <scottwestover2006@gmail.com>

---
+++ src/game-objects/player/player.ts
@@ -0,0 +1,24 @@
+import * as Phaser from 'phaser';
+import { Position } from '../../common/types';
+import { PLAYER_ANIMATION_KEYS } from '../../common/assets';
+
+export type PlayerConfig = {
+  scene: Phaser.Scene;
+  position: Position;
+  assetKey: string;
+  frame?: number;
+};
+
+export class Player extends Phaser.Physics.Arcade.Sprite {
+  constructor(config: PlayerConfig) {
+    const { scene, position, assetKey, frame } = config;
+    const { x, y } = position;
+    super(scene, x, y, assetKey, frame || 0);
+
+    // add object to scene and enable phaser physics
+    scene.add.existing(this);
+    scene.physics.add.existing(this);
+
+    this.play({ key: PLAYER_ANIMATION_KEYS.IDLE_DOWN, repeat: -1 });
+  }
+}
