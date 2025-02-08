0c8d50825ba65168acc6a124f7f9ebb5edcc260d
Scott Westover <scottwestover2006@gmail.com>
2025-02-08 15:31:44 -0500

    feat: player input testing - finished code
    Signed-off-by: Scott Westover <scottwestover2006@gmail.com>

--- src/game-objects/player/player.ts
+++ src/game-objects/player/player.ts
@@ -1,15 +1,19 @@
 import * as Phaser from 'phaser';
 import { Position } from '../../common/types';
 import { PLAYER_ANIMATION_KEYS } from '../../common/assets';
+import { InputComponent } from '../../components/input/input-component';
 
 export type PlayerConfig = {
   scene: Phaser.Scene;
   position: Position;
   assetKey: string;
   frame?: number;
+  controls: InputComponent;
 };
 
 export class Player extends Phaser.Physics.Arcade.Sprite {
+  #controls: InputComponent;
+
   constructor(config: PlayerConfig) {
     const { scene, position, assetKey, frame } = config;
     const { x, y } = position;
@@ -19,6 +23,37 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
     scene.add.existing(this);
     scene.physics.add.existing(this);
 
+    // add components
+    this.#controls = config.controls;
+
     this.play({ key: PLAYER_ANIMATION_KEYS.IDLE_DOWN, repeat: -1 });
+
+    // enable auto update functionality
+    config.scene.events.on(Phaser.Scenes.Events.UPDATE, this.update, this);
+    config.scene.events.once(
+      Phaser.Scenes.Events.SHUTDOWN,
+      () => {
+        config.scene.events.off(Phaser.Scenes.Events.UPDATE, this.update, this);
+      },
+      this,
+    );
+  }
+
+  update(): void {
+    // vertical movement
+    if (this.#controls.isUpDown) {
+      this.play({ key: PLAYER_ANIMATION_KEYS.IDLE_UP, repeat: -1 }, true);
+    } else if (this.#controls.isDownDown) {
+      this.play({ key: PLAYER_ANIMATION_KEYS.IDLE_DOWN, repeat: -1 }, true);
+    }
+
+    // horizontal movement
+    if (this.#controls.isLeftDown) {
+      this.setFlipX(true);
+      this.play({ key: PLAYER_ANIMATION_KEYS.IDLE_SIDE, repeat: -1 }, true);
+    } else if (this.#controls.isRightDown) {
+      this.setFlipX(false);
+      this.play({ key: PLAYER_ANIMATION_KEYS.IDLE_SIDE, repeat: -1 }, true);
+    }
   }
 }
