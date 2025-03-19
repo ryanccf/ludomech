81956d3975da3475f55e5ad6b2b765f83591bd71
Scott Westover <scottwestover2006@gmail.com>
2025-03-19 08:12:39 -0400

    feat: tiled json data - finished code
    Signed-off-by: Scott Westover <scottwestover2006@gmail.com>

--- src/scenes/game-scene.ts
+++ src/scenes/game-scene.ts
@@ -13,6 +13,17 @@ import { Chest } from '../game-objects/objects/chest';
 import { GameObject, LevelData } from '../common/types';
 import { CUSTOM_EVENTS, EVENT_BUS } from '../common/event-bus';
 import { isArcadePhysicsBody } from '../common/utils';
+import { TiledRoomObject } from '../common/tiled/types';
+import { TILED_LAYER_NAMES } from '../common/tiled/common';
+import {
+  getAllLayerNamesWithPrefix,
+  getTiledChestObjectsFromMap,
+  getTiledDoorObjectsFromMap,
+  getTiledEnemyObjectsFromMap,
+  getTiledPotObjectsFromMap,
+  getTiledRoomObjectsFromMap,
+  getTiledSwitchObjectsFromMap,
+} from '../common/tiled/tiled-utils';
 
 export class GameScene extends Phaser.Scene {
   #levelData!: LevelData;
@@ -21,6 +32,18 @@ export class GameScene extends Phaser.Scene {
   #enemyGroup!: Phaser.GameObjects.Group;
   #blockingGroup!: Phaser.GameObjects.Group;
   #potGameObjects!: Pot[];
+  #objectsByRoomId!: {
+    [key: number]: {
+      chestMap: { [key: number]: Chest };
+      doorMap: { [key: number]: unknown };
+      doors: unknown[];
+      switches: unknown[];
+      pots: Pot[];
+      chests: Chest[];
+      enemyGroup?: Phaser.GameObjects.Group;
+      room: TiledRoomObject;
+    };
+  };
 
   constructor() {
     super({
@@ -123,8 +146,39 @@ export class GameScene extends Phaser.Scene {
   }
 
   #createLevel(): void {
+    // create main background
     this.add.image(0, 0, ASSET_KEYS[`${this.#levelData.level}_BACKGROUND`], 0).setOrigin(0);
+    // create main foreground
     this.add.image(0, 0, ASSET_KEYS[`${this.#levelData.level}_FOREGROUND`], 0).setOrigin(0).setDepth(2);
+
+    // create tilemap from Tiled json data
+    const map = this.make.tilemap({
+      key: ASSET_KEYS[`${this.#levelData.level}_LEVEL`],
+    });
+
+    // initialize objects
+    this.#objectsByRoomId = {};
+
+    // create game objects
+    this.#createRooms(map, TILED_LAYER_NAMES.ROOMS);
+
+    const rooms = getAllLayerNamesWithPrefix(map, TILED_LAYER_NAMES.ROOMS).map((layerName: string) => {
+      return {
+        name: layerName,
+        roomId: parseInt(layerName.split('/')[1], 10),
+      };
+    });
+    const switchLayerNames = rooms.filter((layer) => layer.name.endsWith(`/${TILED_LAYER_NAMES.SWITCHES}`));
+    const potLayerNames = rooms.filter((layer) => layer.name.endsWith(`/${TILED_LAYER_NAMES.POTS}`));
+    const doorLayerNames = rooms.filter((layer) => layer.name.endsWith(`/${TILED_LAYER_NAMES.DOORS}`));
+    const chestLayerNames = rooms.filter((layer) => layer.name.endsWith(`/${TILED_LAYER_NAMES.CHESTS}`));
+    const enemyLayerNames = rooms.filter((layer) => layer.name.endsWith(`/${TILED_LAYER_NAMES.ENEMIES}`));
+
+    doorLayerNames.forEach((layer) => this.#createDoors(map, layer.name, layer.roomId));
+    switchLayerNames.forEach((layer) => this.#createButtons(map, layer.name, layer.roomId));
+    potLayerNames.forEach((layer) => this.#createPots(map, layer.name, layer.roomId));
+    chestLayerNames.forEach((layer) => this.#createChests(map, layer.name, layer.roomId));
+    enemyLayerNames.forEach((layer) => this.#createEnemies(map, layer.name, layer.roomId));
   }
 
   #setupCamera(): void {
@@ -180,5 +234,72 @@ export class GameScene extends Phaser.Scene {
         requiresBossKey: true,
       }),
     ]);
+  }
+
+  /**
+   * Parses the Tiled Map data and creates the 'Room' game objects
+   * from the rooms layer in Tiled. The `Room` object is how we group
+   * the various game objects in our game.
+   */
+  #createRooms(map: Phaser.Tilemaps.Tilemap, layerName: string): void {
+    const validTiledObjects = getTiledRoomObjectsFromMap(map, layerName);
+    validTiledObjects.forEach((tiledObject) => {
+      this.#objectsByRoomId[tiledObject.id] = {
+        switches: [],
+        pots: [],
+        doors: [],
+        chests: [],
+        room: tiledObject,
+        chestMap: {},
+        doorMap: {},
+      };
+    });
+  }
+
+  /**
+   * Parses the Tiled Map data and creates the 'Door' game objects
+   * for transitions between the various rooms/caves/buildings/etc.
+   */
+  #createDoors(map: Phaser.Tilemaps.Tilemap, layerName: string, roomId: number): void {
+    console.log(layerName, roomId);
+    const validTiledObjects = getTiledDoorObjectsFromMap(map, layerName);
+    console.log(validTiledObjects);
+  }
+
+  /**
+   * Parses the Tiled Map data and creates the 'Button' game objects
+   * that players can interact with to open doors, reveal chests, etc.
+   */
+  #createButtons(map: Phaser.Tilemaps.Tilemap, layerName: string, roomId: number): void {
+    console.log(layerName, roomId);
+    const validTiledObjects = getTiledSwitchObjectsFromMap(map, layerName);
+    console.log(validTiledObjects);
+  }
+
+  /**
+   * Parses the Tiled Map data and creates the 'Pot' game objects.
+   */
+  #createPots(map: Phaser.Tilemaps.Tilemap, layerName: string, roomId: number): void {
+    console.log(layerName, roomId);
+    const validTiledObjects = getTiledPotObjectsFromMap(map, layerName);
+    console.log(validTiledObjects);
+  }
+
+  /**
+   * Parses the Tiled Map data and creates the 'Chest' game objects.
+   */
+  #createChests(map: Phaser.Tilemaps.Tilemap, layerName: string, roomId: number): void {
+    console.log(layerName, roomId);
+    const validTiledObjects = getTiledChestObjectsFromMap(map, layerName);
+    console.log(validTiledObjects);
+  }
+
+  /**
+   * Parses the Tiled Map data and creates the various enemy game objects like 'Wisp' and 'Spider'.
+   */
+  #createEnemies(map: Phaser.Tilemaps.Tilemap, layerName: string, roomId: number): void {
+    console.log(layerName, roomId);
+    const validTiledObjects = getTiledEnemyObjectsFromMap(map, layerName);
+    console.log(validTiledObjects);
   }
 }
